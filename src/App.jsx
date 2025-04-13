775b6dbd674f5cbb962641ca0719f19942c3431f
Bishal-np <bisyalb0998@gmail.com>
2025-04-13 19:56:23 +0545

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,16 @@ import Banner from './componant/layouts/Banner'
 import About from './componant/layouts/About'
 import Services from './componant/layouts/Services'
 import Contact from './componant/layouts/Contact'
+
 function App() {
   return (
     <>
       <Header />
       <Banner />
       <About />
+     
       <Services />
+
       <Contact />
       <Footer />
     </>
--- src/componant/layouts/About.jsx
+++ src/componant/layouts/About.jsx
@@ -10,28 +10,33 @@ function About() {
           <div className="my-details d-flex ">
             <div className="intro col-lg-6 col-md-12">
               <p className="dec">
-                
+
               </p>
               <button className="cv"><MdOutlineFileDownload /> Download</button>
             </div>
             <div className="skill  col-lg-6 col-md-12">
               <h2>Skills </h2>
-              <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                <div className="progress-bar text-bg-success" >25%</div>
+              <div className="progress " role="progressbar" aria-label="Success example" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                <div className="progress-bar text-bg-success" >HTML  80%</div>
               </div>
-              <div className="progress" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
-                <div className="progress-bar text-bg-info" >50%</div>
+              <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+                <div className="progress-bar text-bg-success" >CSS  70%</div>
               </div>
-              <div className="progress" role="progressbar" aria-label="Warning example" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">
-                <div className="progress-bar text-bg-warning" >75%</div>
+              <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+                <div className="progress-bar text-bg-success" >JAVA SCRIPT 60%</div>
               </div>
-              <div className="progress" role="progressbar" aria-label="Danger example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-                <div className="progress-bar text-bg-danger" >100%</div>
+              <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+                <div className="progress-bar text-bg-success" >BOOTSTRAP 70%</div>
               </div>
+              <div className="progress" role="progressbar" aria-label="Success example" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+                <div className="progress-bar text-bg-success" >REACT 50%</div>
+              </div>
+
 
 
 
             </div>
+
           </div>
 
         </div>
