8b245df049caf2adc15a64e7078edd4d2b78c0b9
Bishal-np <bisyalb0998@gmail.com>
2025-04-13 12:18:43 +0545

    header update

--- src/componant/Header.jsx
+++ src/componant/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import "../scss/layouts/header.scss";
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
+ 
 
   useEffect(() => {
     const handleScroll = () => {
@@ -24,25 +25,25 @@ function Header() {
     <>
 
 
-      <header className={`navbar navbar-expand-lg py-3 bg-body-dark ${isScrolled ? 'scrolled' : ''}`}>
-        
+      <header className={`navbar navbar-expand-lg py-4 bg-body-dark ${isScrolled ? 'scrolled' : ''}`}>
+
         <div className="container">
 
-        <div className="logo"><a href="/home">BN.</a></div>
-        <button className="navbar-toggler text-light " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon  " />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className='d-flex mb-0 ms-auto gap-4 align-items-center menu-items'>
-            <li><a href={`#home`}>Home</a></li>
-            <li><a href={`#about`}>Who I Am</a></li>
-            <li><a href={`#services`}>What I Offer</a></li>
-            <li><a href={`#experiences`}>Experiences</a></li>
-            <li><a href={`#blog`}>Blog</a></li>
-            <li><a href={`#contact`}>Let's Talk!</a></li>
-          </ul>
+          <div className="logo"><a href="/home">BN.</a></div>
+          <button className="navbar-toggler  " id='nav-bar-toggle' type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon "  />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className='d-flex mb-0 ms-auto gap-4 align-items-center menu-items' id='menu_list'>
+              <li><a href={`#home`}>Home</a></li>
+              <li><a href={`#about`}>About Me</a></li>
+              <li><a href={`#services`}>Resume</a></li>
+              <li><a href={`#experiences`}>Projects</a></li>
+              <li><a href={`#blog`}>Blog</a></li>
+              <li><a href={`#contact`}>Contact</a></li>
+            </ul>
 
-        </div>
+          </div>
         </div>
       </header>
     </>
