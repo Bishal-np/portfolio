1a56661ffbe9167d93c3482dd78c1260eb1d2398
Bishal-np <bisyalb0998@gmail.com>
2025-04-13 18:32:41 +0545

    all update

--- src/componant/layouts/About.jsx
+++ src/componant/layouts/About.jsx
@@ -1,18 +1,36 @@
 import React from 'react'
+import { MdOutlineFileDownload } from "react-icons/md";
 
 function About() {
   return (
     <>
       <section className="about-section" id='about'>
-        <section className="about-section-1 container">
-          <h2 className='about-heading'> Who I Am</h2>
-          <div className="about">
-            <p>I’m a Front-End Developer & WordPress Specialist passionate about crafting clean, responsive websites. With expertise in HTML, CSS, JavaScript, and custom WordPress themes, I focus on user-friendly, high-performance designs that bring ideas to life. <br />
-              Let’s collaborate! Whether you need a sleek portfolio or a dynamic business site, I’ll turn your vision into reality—efficiently and beautifully.</p>
-            <div>
-              <button className="download-cv"><a href="public/bishal-nepali-cv (1).pdf">Download CV</a></button></div>
+        <div className="container about-me">
+          <h2 className="heading">About Me</h2>
+          <div className="my-details d-flex">
+            <div className="intro">
+              <p className="dec">I’m a Front-End Developer & WordPress Specialist
+                passionate about crafting clean,
+                responsive websites. With expertise in HTML, CSS, JavaScript, and custom WordPress themes,
+                I focus on user-friendly, high-performance designs that bring ideas to life. <br />
+                Let’s collaborate! Whether you need a sleek portfolio
+                or a dynamic business site, I’ll turn your vision into
+                reality—efficiently and beautifully.
+              </p>
+              <button className="cv"><MdOutlineFileDownload /> Download</button>
+            </div>
+            <div className="skill ">
+              <h2>second </h2>
+             
+
+
+            </div>
           </div>
-        </section>
+
+        </div>
+
+
+
 
       </section>
 
--- src/componant/layouts/Services.jsx
+++ src/componant/layouts/Services.jsx
@@ -5,16 +5,16 @@ function Services() {
         <>
             <section className="services" id='services'>
                 <div className="services-section container">
-                    <h2 className='text-center pt-5  srv-heading '> What I Offer</h2>
+                    <h2 className='text-center pt-5  srv-heading '> services </h2>
                     <p className='text-center service-sub-heading pb-5'>
                         Modern, responsive front-end development using React.js for fast, scalable web applications and custom WordPress design and development, from sleek themes to seamless functionality
                     </p>
                     <div className="row gy-4">
                         <div className="col-lg-6 col-md-12">
-                            <div className="card border-0 ">
-                                <div className="card-body text-bg-success">
+                            <div className="card text-bg-success">
+                                <div className="card-body ">
                                     <div className=' icon'>
-                                    <i className="bi bi-phone-vibrate"></i>
+                                    <i class="bi bi-front"></i>
                                     </div>
                                     <h5 className="card-title card-heading">Front-End Development</h5>
                                     <p className="card-text card-dec">I craft fast, modern websites using HTML, CSS, JS, jQuery & React. With clean code and React Hooks, I build responsive, interactive sites for any device. Let’s create!</p>
@@ -22,40 +22,16 @@ function Services() {
                             </div>
                         </div>
                         <div className="col-lg-6 col-md-12">
-                            <div className="card ">
-                                <div className="card-body text-bg-warning">
+                            <div className="card text-bg-primary">
+                                <div className="card-body ">
                                     <div className=' icon'>
-                                        <i className="bi bi-phone-vibrate"></i>
+                                    <i class="bi bi-wordpress"></i>
                                     </div>
-                                    <h5 className="card-title card-heading text-light">Wordpress Design</h5>
-                                    <p className="card-text card-dec-1 text-light">I design fast, responsive WordPress websites with custom themes, seamless UX, and easy-to-manage CMS. Let's build your perfect site together!</p>
+                                    <h5 className="card-title card-heading">Wordpress Development</h5>
+                                    <p className="card-text card-dec">I design fast, responsive WordPress websites with custom themes, seamless UX, and easy-to-manage CMS. Let's build your perfect site together!</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-6 col-md-12">
-                            <div className="card border-0">
-                                <div className="card-body text-bg-primary">
-                                    <div className=' icon'>
-                                        <i className="bi bi-phone-vibrate"></i>
-                                    </div>
-                                    <h5 className="card-title card-heading text-light">UI/UX Design</h5>
-                                    <p className="card-text card-dec-2 text-light">I design intuitive UI/UX in Figma, crafting user-friendly interfaces with sleek visuals and seamless workflows. Let’s elevate your digital experience!</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 col-md-12">
-                            <div className="card border-0">
-                                <div className="card-body text-bg-danger">
-                                    <div className=' icon'>
-                                        <i className="bi bi-phone-vibrate"></i>
-                                    </div>
-                                    <h5 className="card-title card-heading text-light">Freelance Work</h5>
-                                    <p className="card-text card-dec-3 text-light">Freelance web & UI/UX solutions: React development, WordPress sites, and Figma designs—all tailored to your needs. Let's collaborate!</p>
-                                </div>
-                            </div>
-                        </div>
-                        
-
                     </div>
                 </div>
             </section>
