f1615f4967d5c03fbac9a88b0ade8eb306ad8839
Bishal-np <bisyalb0998@gmail.com>
2025-04-16 18:09:04 +0545

    footer update

--- src/componant/Footer.jsx
+++ src/componant/Footer.jsx
@@ -4,32 +4,26 @@ import "../scss/layouts/footer.scss"
 function Footer() {
   return (
     <footer className='footer'>
-      <h2 className='logo'>BN.</h2>
-      <div>
-        <p className='description'>Follow Me</p>
-        <div className="social-icons">
-          <ul className='d-flex gap-5  align-items-center  justify-content-center fs-5 text-light icons'>
-            <li><a href=""><i className="bi bi-facebook"></i></a></li>
-            <li><a href=""><i className="bi bi-pinterest"></i></a></li>
-            <li><a href=""><i className="bi bi-linkedin"></i></a></li>
-            <li><a href=""><i className="bi bi-github"></i></a></li>
-            <li><a href=""><i className="bi bi-whatsapp"></i></a></li>
-          </ul>
-        </div>
-        <div className="copy-right">
-          <div className="container copy-right-section">
-            <p className='copy'>Copyright © 2025 BishalNepali</p>
+      <div className="footer-content">
+        <h2 className='logo'>BN.</h2>
+        <div className="footer-main">
+          <p className='description'>Follow Me</p>
+          <div className="social-icons">
+            <ul className='social-links'>
+              <li><a href="#"><i className="bi bi-facebook"></i></a></li>
+              <li><a href="#"><i className="bi bi-pinterest"></i></a></li>
+              <li><a href="#"><i className="bi bi-linkedin"></i></a></li>
+              <li><a href="#"><i className="bi bi-github"></i></a></li>
+              <li><a href="#"><i className="bi bi-whatsapp"></i></a></li>
+            </ul>
+          </div>
+          <div className="copy-right">
+            <div className="copy-right-section">
+              <p className='copy'>Copyright © 2025 BishalNepali</p>
+            </div>
           </div>
         </div>
       </div>
-
-
-
-
-
-
-
-
     </footer>
   )
 }
