8abfc257391638e26cd7d8226d2bd2e604242b93
Bishal-np <bisyalb0998@gmail.com>
2025-04-16 16:21:14 +0545

    header responsive

--- src/componant/Header.jsx
+++ src/componant/Header.jsx
@@ -1,50 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import "../scss/layouts/header.scss";
+
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
- 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
+      setIsScrolled(window.scrollY > 50);
     };
 
     window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
-    <>
-      <header className={`navbar navbar-expand-lg py-4 bg-body-dark ${isScrolled ? 'scrolled' : ''}`}>
-
-        <div className="container">
-
-          <div className="logo"><a href="/home">BN.</a></div>
-          <button className="navbar-toggler  " id='nav-bar-toggle' type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon "  />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className='d-flex mb-0 ms-auto gap-4 align-items-center menu-items' id='menu_list'>
-              <li><a href={`#home`}>Home</a></li>
-              <li><a href={`#about`}>About Me</a></li>
-              <li><a href={`#services`}>Resume</a></li>
-              <li><a href={`#experiences`}>Projects</a></li>
-              <li><a href={`#blog`}>Blog</a></li>
-              <li><a href={`#contact`}>Contact</a></li>
-            </ul>
-
-          </div>
+    <header className={`navbar navbar-expand-lg py-4 bg-body-dark ${isScrolled ? 'scrolled' : ''}`}>
+      <div className="container">
+        <div className="logo"><a href="/home">BN.</a></div>
+        
+        {/* Responsive Toggle Button */}
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          onClick={toggleMenu}
+          aria-expanded={isMenuOpen}
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        
+        {/* Responsive Menu */}
+        <div className={`collapse navbar-collapse ${isMenuOpen ? 'show' : ''}`} id="navbarSupportedContent">
+          <ul className="mb-0 ms-auto gap-4 align-items-center menu-items" id="menu_list">
+            {['home', 'about', 'services', 'experiences', 'blog', 'contact'].map((item) => (
+              <li key={item} onClick={closeMenu}>
+                <a href={`#${item}`}>
+                  {item.charAt(0).toUpperCase() + item.slice(1)}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 }
+
 export default Header;
